4850f0d992b088606cb70b14f6337370dbe48b6d
JessC418819 <418819@nelsongroup.ac.uk>
2025-04-30 21:54:35 +0100

    Update main.py
    Updated shoppingDict and user inputs added for peer review by Ben Harrison

--- main.py
+++ main.py
@@ -51,11 +51,57 @@ priceDict = {
 #This code uses the 'print' function to display the dictionary on screen
 print(priceDict)
 
+#AMENDED DICTIONARY - JESS
+
+#This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
+shoppingDict = {
+  "001": ("Margarita Pizza", 10.00),
+  "002": ("Pepperoni Pizza", 10.00),
+  "003": ("Beef Burger", 5.00),
+  "004": ("Chicken Burger", 5.00),
+  "005": ("Chips", 5.00),
+  "006": ("Add Bacon", 1.50),
+  "007": ("Add Cheese", 0.75),
+  "008": ("Hot Drink - Tea", 1.50),
+  "009": ("Hot Drink - Coffee", 1.50),
+  "010": ("Cold Drink - Coke", 2.25),
+  "011": ("Cold Drink - Fanta", 2.25),
+  "012": ("Cold Drink - 7up", 2.25),
+  "013": ("Ice Cream", 0.99),
+  "014": ("Wind Break", 34.99),
+  "015": ("Sun Umbrella", 22.50),
+  "016": ("Mini BBQ", 7.00),
+  "017": ("Portable TV", 154.97)
+}
+#This code uses the 'print' function to display the dictionary on screen
+print(shoppingDict)
+
+
 """
 Module: 2 - User inputs for item code and QTY
 Developer: Jess
 Tester: Ben
 Status: Needs attention, see Ben's GH comments
+
+#ADDED USER INPUTS - JESS
+
+#This code asks the user to select which item they would like to purchase
+input("Type the item code of the item you'd like to purchase here: ")
+
+#This code asks the user to confirm how many of the chosen item they'd wish to purchase. The IF ELSE selection statement has been placed in here to ensure the user chooses at least one or more 
+quantity = input("How many would you like? Type here: ")
+  
+if quantity >= "1":
+  print("Do you want to add another item?")
+  input("Y or N: ")
+else:
+  print("Enter a valid input of either 1 or more"), input(" ")
+  print("Do you want to add another item?"), input("Y or N: ")
+  
+
+#A WHILE LOOP is required to continue this cycle of questioning until the user decides 'N' they don't wish to add any more items to their shopping cart
+
+
 """
 
 #This code asks the user to select which item they would like to purchase and confirm the quantity
@@ -114,3 +160,4 @@ print (veriable_of_receipt)
 #This will display the contents of the variable to the user in the conosole 
 receipt.close()
 #This will close the text files. 
+
