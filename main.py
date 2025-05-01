eff28337a8f8d14c42e3514fb6369cbd920280d2
JessC418819 <418819@nelsongroup.ac.uk>
2025-05-01 09:54:09 +0100

    Update main.py
    Amended dictionary v4 for peer review, thanks

--- main.py
+++ main.py
@@ -105,6 +105,34 @@ for key, values in shoppingDict.items():
   print(key)
   print (values)
 
+#AMENDED DICTIONARY v4 - JESS 
+
+#This code creates the key:value pairings for the 'shoppingDict', which, when displayed to the user offers all items available to purchase
+shoppingDict = {
+  "001": ("Margarita Pizza", 10.00),
+  "002": ("Pepperoni Pizza", 10.00),
+  "003": ("Beef Burger", 5.00),
+  "004": ("Chicken Burger", 5.00),
+  "005": ("Chips", 5.00),
+  "006": ("Add Bacon", 1.50),
+  "007": ("Add Cheese", 0.75),
+  "008": ("Hot Drink - Tea", 1.50),
+  "009": ("Hot Drink - Coffee", 1.50),
+  "010": ("Cold Drink - Coke", 2.25),
+  "011": ("Cold Drink - Fanta", 2.25),
+  "012": ("Cold Drink - 7up", 2.25),
+  "013": ("Ice Cream", 0.99),
+  "014": ("Wind Break", 34.99),
+  "015": ("Sun Umbrella", 22.50),
+  "016": ("Mini BBQ", 7.00),
+  "017": ("Portable TV", 154.97)
+}
+
+#This code uses a FOR LOOP alongside the 'print' function to display the dictionary on screen, line by line for ease of viewing. It also uses an F STRING which allows the code to become dynamic i.e. if the value changes, the code will change with it
+for key, values in shoppingDict.items():
+  print(key)
+  print(f"{values[0]}: £{values[1]:.2f}")
+
 
 """
 Module: 2 - User inputs for item code and QTY
