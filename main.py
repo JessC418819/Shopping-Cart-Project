7a2669a3707b8704e330ba9e6c033009b3af5a0a
JessC418819 <418819@nelsongroup.ac.uk>
2025-05-01 09:34:56 +0100

    Update main.py
    Amended dictionary v3 for peer review, thanks

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Tester: Ben
 Status: Needs attention, see Ben's GH comments 30/04/2025
 """
 
+#INITIAL DICTIONARY v1 - JESS
+
 #This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
 itemDict = {
   "001M": "Margarita Pizza",
@@ -51,7 +53,7 @@ priceDict = {
 #This code uses the 'print' function to display the dictionary on screen
 print(priceDict)
 
-#AMENDED DICTIONARY - JESS
+#AMENDED DICTIONARY v2 - JESS
 
 #This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
 shoppingDict = {
@@ -75,6 +77,33 @@ shoppingDict = {
 }
 #This code uses the 'print' function to display the dictionary on screen
 print(shoppingDict)
+
+#AMENDED DICTIONARY v3 - JESS
+
+#This code creates the key:value pairings for the 'shoppingDict', which, when displayed to the user offers all items available to purchase
+shoppingDict = {
+  "001": ("Margarita Pizza", 10.00),
+  "002": ("Pepperoni Pizza", 10.00),
+  "003": ("Beef Burger", 5.00),
+  "004": ("Chicken Burger", 5.00),
+  "005": ("Chips", 5.00),
+  "006": ("Add Bacon", 1.50),
+  "007": ("Add Cheese", 0.75),
+  "008": ("Hot Drink - Tea", 1.50),
+  "009": ("Hot Drink - Coffee", 1.50),
+  "010": ("Cold Drink - Coke", 2.25),
+  "011": ("Cold Drink - Fanta", 2.25),
+  "012": ("Cold Drink - 7up", 2.25),
+  "013": ("Ice Cream", 0.99),
+  "014": ("Wind Break", 34.99),
+  "015": ("Sun Umbrella", 22.50),
+  "016": ("Mini BBQ", 7.00),
+  "017": ("Portable TV", 154.97)
+}
+#This code uses a FOR LOOP alongside a 'print' function to display the dictionary on screen, line by line for ease of viewing
+for key, values in shoppingDict.items():
+  print(key)
+  print (values)
 
 
 """
