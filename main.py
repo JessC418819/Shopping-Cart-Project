aae467d9a51ab40a1f35d6a21fd7427daeebaa75
JessC418819 <418819@nelsongroup.ac.uk>
2025-05-03 22:00:17 +0100

    Update main.py
    Previous versions of code which have been amended have been #commented out so that they don't run in the program

--- main.py
+++ main.py
@@ -2,114 +2,114 @@ from datetime import datetime        # Imports the datetime module
 today = datetime.now()               # Calls the now function, which gets the current time/date from the system clock --> 
 print(today)                         # Print statement to display time --> Nathan to move this line of code into module 6 once tested
 
-"""
-Module: 1 - Create dictionary
-Developer: Jess
-Tester: Ben
-Status: Needs attention, see Ben's GH comments 30/04/2025
-"""
+# """
+# Module: 1 - Create dictionary
+# Developer: Jess
+# Tester: Ben
+# Status: Needs attention, see Ben's GH comments 30/04/2025
+# """
 
-#INITIAL DICTIONARY v1 - JESS
+# #INITIAL DICTIONARY v1 - JESS
 
-#This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
-itemDict = {
-  "001M": "Margarita Pizza",
-  "001P": "Pepperoni Pizza",
-  "002B": "Beef Burger",
-  "002C": "Chicken Burger",
-  "003": "Chips",
-  "004": "Add Bacon",
-  "005": "Add Cheese",
-  "006T": "Hot Drink - Tea",
-  "006C": "Hot Drink - Coffee",
-  "007C": "Cold Drink - Coke",
-  "007F": "Cold Drink - Fanta",
-  "0077": "Cold Drink - 7up",
-  "008": "Ice Cream",
-  "009": "Wind Break",
-  "010": "Sun Umbrella",
-  "011": "Mini BBQ",
-  "012": "Portable TV"
-}
-#This code uses the 'print' function to display the dictionary on screen
-print(itemDict)
+# #This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
+# itemDict = {
+#   "001M": "Margarita Pizza",
+#   "001P": "Pepperoni Pizza",
+#   "002B": "Beef Burger",
+#   "002C": "Chicken Burger",
+#   "003": "Chips",
+#   "004": "Add Bacon",
+#   "005": "Add Cheese",
+#   "006T": "Hot Drink - Tea",
+#   "006C": "Hot Drink - Coffee",
+#   "007C": "Cold Drink - Coke",
+#   "007F": "Cold Drink - Fanta",
+#   "0077": "Cold Drink - 7up",
+#   "008": "Ice Cream",
+#   "009": "Wind Break",
+#   "010": "Sun Umbrella",
+#   "011": "Mini BBQ",
+#   "012": "Portable TV"
+# }
+# #This code uses the 'print' function to display the dictionary on screen
+# print(itemDict)
 
-#This code creates the key:value pairings for the 'priceDict', which, when displayed to the user offers the cost of each item
-priceDict = {
-  "001M": 10.00,
-  "001P": 10.00,
-  "002B": 5.00,
-  "002C": 5.00,
-  "003": 5.00,
-  "004": 1.50,
-  "005": 0.75,
-  "006T": 1.50,
-  "006C": 1.50,
-  "007C": 2.25,
-  "007F": 2.25,
-  "0077": 2.25,
-  "008": 0.99,
-  "009": 34.99,
-  "010": 22.50,
-  "011": 7.00,
-  "012": 154.97
-}
-#This code uses the 'print' function to display the dictionary on screen
-print(priceDict)
+# #This code creates the key:value pairings for the 'priceDict', which, when displayed to the user offers the cost of each item
+# priceDict = {
+#   "001M": 10.00,
+#   "001P": 10.00,
+#   "002B": 5.00,
+#   "002C": 5.00,
+#   "003": 5.00,
+#   "004": 1.50,
+#   "005": 0.75,
+#   "006T": 1.50,
+#   "006C": 1.50,
+#   "007C": 2.25,
+#   "007F": 2.25,
+#   "0077": 2.25,
+#   "008": 0.99,
+#   "009": 34.99,
+#   "010": 22.50,
+#   "011": 7.00,
+#   "012": 154.97
+# }
+# #This code uses the 'print' function to display the dictionary on screen
+# print(priceDict)
 
-#AMENDED DICTIONARY v2 - JESS
+# #AMENDED DICTIONARY v2 - JESS
 
-#This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
-shoppingDict = {
-  "001": ("Margarita Pizza", 10.00),
-  "002": ("Pepperoni Pizza", 10.00),
-  "003": ("Beef Burger", 5.00),
-  "004": ("Chicken Burger", 5.00),
-  "005": ("Chips", 5.00),
-  "006": ("Add Bacon", 1.50),
-  "007": ("Add Cheese", 0.75),
-  "008": ("Hot Drink - Tea", 1.50),
-  "009": ("Hot Drink - Coffee", 1.50),
-  "010": ("Cold Drink - Coke", 2.25),
-  "011": ("Cold Drink - Fanta", 2.25),
-  "012": ("Cold Drink - 7up", 2.25),
-  "013": ("Ice Cream", 0.99),
-  "014": ("Wind Break", 34.99),
-  "015": ("Sun Umbrella", 22.50),
-  "016": ("Mini BBQ", 7.00),
-  "017": ("Portable TV", 154.97)
-}
-#This code uses the 'print' function to display the dictionary on screen
-print(shoppingDict)
+# #This code creates the key:value pairings for the 'itemDict', which, when displayed to the user offers all items available to purchase
+# shoppingDict = {
+#   "001": ("Margarita Pizza", 10.00),
+#   "002": ("Pepperoni Pizza", 10.00),
+#   "003": ("Beef Burger", 5.00),
+#   "004": ("Chicken Burger", 5.00),
+#   "005": ("Chips", 5.00),
+#   "006": ("Add Bacon", 1.50),
+#   "007": ("Add Cheese", 0.75),
+#   "008": ("Hot Drink - Tea", 1.50),
+#   "009": ("Hot Drink - Coffee", 1.50),
+#   "010": ("Cold Drink - Coke", 2.25),
+#   "011": ("Cold Drink - Fanta", 2.25),
+#   "012": ("Cold Drink - 7up", 2.25),
+#   "013": ("Ice Cream", 0.99),
+#   "014": ("Wind Break", 34.99),
+#   "015": ("Sun Umbrella", 22.50),
+#   "016": ("Mini BBQ", 7.00),
+#   "017": ("Portable TV", 154.97)
+# }
+# #This code uses the 'print' function to display the dictionary on screen
+# print(shoppingDict)
 
-#AMENDED DICTIONARY v3 - JESS
+# #AMENDED DICTIONARY v3 - JESS
 
-#This code creates the key:value pairings for the 'shoppingDict', which, when displayed to the user offers all items available to purchase
-shoppingDict = {
-  "001": ("Margarita Pizza", 10.00),
-  "002": ("Pepperoni Pizza", 10.00),
-  "003": ("Beef Burger", 5.00),
-  "004": ("Chicken Burger", 5.00),
-  "005": ("Chips", 5.00),
-  "006": ("Add Bacon", 1.50),
-  "007": ("Add Cheese", 0.75),
-  "008": ("Hot Drink - Tea", 1.50),
-  "009": ("Hot Drink - Coffee", 1.50),
-  "010": ("Cold Drink - Coke", 2.25),
-  "011": ("Cold Drink - Fanta", 2.25),
-  "012": ("Cold Drink - 7up", 2.25),
-  "013": ("Ice Cream", 0.99),
-  "014": ("Wind Break", 34.99),
-  "015": ("Sun Umbrella", 22.50),
-  "016": ("Mini BBQ", 7.00),
-  "017": ("Portable TV", 154.97)
-}
-#This code uses a FOR LOOP alongside a 'print' function to display the dictionary on screen, line by line for ease of viewing
-for key, values in shoppingDict.items():
-  print(key)
-  print (values)
+# #This code creates the key:value pairings for the 'shoppingDict', which, when displayed to the user offers all items available to purchase
+# shoppingDict = {
+#   "001": ("Margarita Pizza", 10.00),
+#   "002": ("Pepperoni Pizza", 10.00),
+#   "003": ("Beef Burger", 5.00),
+#   "004": ("Chicken Burger", 5.00),
+#   "005": ("Chips", 5.00),
+#   "006": ("Add Bacon", 1.50),
+#   "007": ("Add Cheese", 0.75),
+#   "008": ("Hot Drink - Tea", 1.50),
+#   "009": ("Hot Drink - Coffee", 1.50),
+#   "010": ("Cold Drink - Coke", 2.25),
+#   "011": ("Cold Drink - Fanta", 2.25),
+#   "012": ("Cold Drink - 7up", 2.25),
+#   "013": ("Ice Cream", 0.99),
+#   "014": ("Wind Break", 34.99),
+#   "015": ("Sun Umbrella", 22.50),
+#   "016": ("Mini BBQ", 7.00),
+#   "017": ("Portable TV", 154.97)
+# }
+# #This code uses a FOR LOOP alongside a 'print' function to display the dictionary on screen, line by line for ease of viewing
+# for key, values in shoppingDict.items():
+#   print(key)
+#   print (values)
 
-#AMENDED DICTIONARY v4 - JESS 
+# USE THIS - AMENDED DICTIONARY v4 - JESS 
 
 #This code creates the key:value pairings for the 'shoppingDict', which, when displayed to the user offers all items available to purchase
 shoppingDict = {
@@ -138,32 +138,32 @@ for key, values in shoppingDict.items():
   print(f"{values[0]}: £{values[1]:.2f}")
 
 
-"""
-Module: 2 - User inputs for item code and QTY
-Developer: Jess
-Tester: Ben
-Status: Needs attention, see Ben's GH comments
+# """
+# Module: 2 - User inputs for item code and QTY
+# Developer: Jess
+# Tester: Ben
+# Status: Needs attention, see Ben's GH comments
 
-#ADDED USER INPUTS - JESS
+# #USER INPUTS v1 - JESS
 
-#This code asks the user to select which item they would like to purchase
-input("Type the item code of the item you'd like to purchase here: ")
+# #This code asks the user to select which item they would like to purchase
+# input("Type the item code of the item you'd like to purchase here: ")
 
-#This code asks the user to confirm how many of the chosen item they'd wish to purchase. The IF ELSE selection statement has been placed in here to ensure the user chooses at least one or more 
-quantity = input("How many would you like? Type here: ")
+# #This code asks the user to confirm how many of the chosen item they'd wish to purchase. The IF ELSE selection statement has been placed in here to ensure the user chooses at least one or more 
+# quantity = input("How many would you like? Type here: ")
   
-if quantity >= "1":
-  print("Do you want to add another item?")
-  input("Y or N: ")
-else:
-  print("Enter a valid input of either 1 or more"), input(" ")
-  print("Do you want to add another item?"), input("Y or N: ")
+# if quantity >= "1":
+#   print("Do you want to add another item?")
+#   input("Y or N: ")
+# else:
+#   print("Enter a valid input of either 1 or more"), input(" ")
+#   print("Do you want to add another item?"), input("Y or N: ")
   
 
-#A WHILE LOOP is required to continue this cycle of questioning until the user decides 'N' they don't wish to add any more items to their shopping cart
+# #A WHILE LOOP is required to continue this cycle of questioning until the user decides 'N' they don't wish to add any more items to their shopping cart
 
 
-#AMENDED USER INPUTS INC. WHILE LOOP AND NESTED IF ELSE SELECTION - JESS
+#AMENDED USER INPUTS v2 INC. WHILE LOOP AND NESTED IF ELSE SELECTION - JESS
 
 #Declare the List 'shoppingCart[]' but leave it empty to be called upon later after the user has chosen their items
 shoppingCart = []
