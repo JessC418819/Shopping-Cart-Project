bdbd3946a6d6b89c89ead3ece9fe1cf192ec8904
aj-mcneill <424603@nelsongroup.ac.uk>
2025-05-11 22:42:02 +0100

    Version 1.0 - Final commit by Adam
    - Menu system implemented to shop and checkout by utilising alignment tools e.g. :<22
    - Comments cleaned up and spell checked
    - Order details and receipt are now worded appropriately (both used to be called "final receipt")

--- main.py
+++ main.py
@@ -1,87 +1,92 @@
-from datetime import datetime # Imports the datetime module (for receipt)
+# Module import
+from datetime import datetime   # Imports the datetime module (for use on the receipt)
+today = datetime.now()          # This assigns datetime to a variable (for use on the receipt)
 
-# Tuple dictionary setup | Jess/Ben: Complete/tested
+# Shopping menu
 shoppingDict = {
-  "001": ("Margherita Pizza", 10.00),
-  "002": ("Pepperoni Pizza", 10.00),
-  "003": ("Beef Burger", 5.00),
-  "004": ("Chicken Burger", 5.00),
-  "005": ("Chips", 5.00),
-  "006": ("Add Bacon", 1.50),
-  "007": ("Add Cheese", 0.75),
-  "008": ("Hot Drink - Tea", 1.50),
-  "009": ("Hot Drink - Coffee", 1.50),
-  "010": ("Cold Drink - Coke", 2.25),
-  "011": ("Cold Drink - Fanta", 2.25),
-  "012": ("Cold Drink - 7up", 2.25),
-  "013": ("Ice Cream", 0.99),
-  "014": ("Wind Break", 34.99),
-  "015": ("Sun Umbrella", 22.50),
-  "016": ("Mini BBQ", 7.00),
-  "017": ("Portable TV", 154.97)
+   "001": ("Margherita Pizza", 10.00), # The dictionary uses the key as the product code, and the tuple values for the item & cost
+   "002": ("Pepperoni Pizza", 10.00),
+   "003": ("Beef Burger", 5.00),
+   "004": ("Chicken Burger", 5.00),
+   "005": ("Chips", 5.00),
+   "006": ("Add Bacon", 1.50),
+   "007": ("Add Cheese", 0.75),
+   "008": ("Hot Drink - Tea", 1.50),
+   "009": ("Hot Drink - Coffee", 1.50),
+   "010": ("Cold Drink - Coke", 2.25),
+   "011": ("Cold Drink - Fanta", 2.25),
+   "012": ("Cold Drink - 7up", 2.25),
+   "013": ("Ice Cream", 0.99),
+   "014": ("Wind Break", 34.99),
+   "015": ("Sun Umbrella", 22.50),
+   "016": ("Mini BBQ", 7.00),
+   "017": ("Portable TV", 154.97)
 }
 
-# Display the shop | Jess/Ben: Complete/tested
-for key, values in shoppingDict.items():  # For loop to print through full dictionary line by line for ease of viewing
-  print(key)                                 
-  print(f"{values[0]}: £{values[1]:.2f}") # F string used to for easier variable inserts. :.2f used for 2 decimal places
+# Display shop menu
+print("Code   | Item                   | Cost (£)")        # Prints the shop menu header
+print("-" * 42)                                            # Prints 38 - as a menu separator
+for key, values in shoppingDict.items():                   # For loop to print through full dictionary, Key=Product Code, Value[0]=Item Name, [Value[1]=Price 
+  print(f"{key:<6} | {values[0]:<22} | {values[1]:>6.2f}") # F string (easier variable management) 2f used for 2 decimal places, proper spacing handled with ><num
+print()
 
-# Setup shopping cart (list) | Jess/Ben: Complete/tested
-shoppingCart = [] # Declare the List 'shoppingCart[]' but leave it empty to be called upon later after the user has chosen their items
+shoppingCart = [] # Declare an empty list which will be called on later (to add the user's items)
 
-# Main Shopping loop | Everyone: Ben to add quantity input validation (see placeholder) | Nathan to add print statement for cart (see placeholder)
-while True: # Opens an infinite loop (it's always True)
-    itemCode = input('Enter the item code of the product you\'d like to purchase here (type "CHECKOUT" to finish): ') # Asks the user to input the item code or "checkout"
+# Main Shopping Loop
+while True: # Opens an infinite loop (always True)
+         
+    # Item code
+    itemCode = input('Enter the item code of the product you\'d like to purchase (type "CHECKOUT" to finish): ').strip() # Asks the user to input the item code or "checkout", uses strip to remove empty spaces from the user's input
 
-    if itemCode.upper() == "CHECKOUT": # Safely handles closure of the infinite loop
-        break # Breaks the loop
-     # Nathan to insert checkout print statement here (element 5)
-  
-    if itemCode not in shoppingDict: # Checks if the user's input (itemCode) is in the dictionary
-        print("Invalid product code, please try again.")
-        continue # Go back to the top of the loop
+    if itemCode.upper() == "CHECKOUT": # Forces the user input (itemCode) to upper case, then checks for the word "checkout"
+        break                          # Breaks the loop and moves onto receipt logic
 
-    quantity = int(input("How many would you like? Type here: "))
-    # Ben to insert quantity handling here (element 4)
+    if itemCode not in shoppingDict:                       # Checks if the user's input (itemCode) is in the dictionary
+        print("Invalid product code, please try again.")   # Informs the user of invalid product code
+        continue                                           # Go back to the top of the loop
+    
+    # Quantity
+    quantity = input("Enter the quantity you would like: ").strip() # Asks the user to input a QTY and removes additional spacing
 
-     # (This was more complicated than you might expect because I had to look up how to deal with what happens if you entered a string as well as entered the wrong quantity - Ben)
-     
-     if quantity.upper() == "CHECKOUT":
-        break
+    if quantity.upper() == "CHECKOUT": # Forces the user input (quantity) to upper case, then checks for the word "checkout"
+      break                            # Breaks the loop and moves onto receipt logic
 
-     try:
-         quantity = int(quantity)
-         while quantity < 1:
-             print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.')
-             continue
+    try:                               # The try block is used for error handling of quantity
+        quantity = int(quantity)       # Converts the user's input to an integer
+        if quantity < 1:               # Checks if the QTY is zero or minus
+          print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.') # Warns the user of invalid quantity
+          continue                     # Go back to the top of the loop
 
-     except ValueError:
-         print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.')
-         continue 
+    except ValueError:                 # If the conversion to integer fails
+        print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.') # Warns the user of invalid quantity
+        continue                       # Go back to the top of the loop
 
-#Is doing everything inside one while block really the best method? I can't suggest anything better but it creates indentation issues further down the line with the 'quantity' variable - Ben
-
+    # Add item to cart
     selectedItem = shoppingDict[itemCode] # Gets item from dictionary (using the item code the user typed)
     shoppingCart.append((selectedItem[0], selectedItem[1], quantity))  # Populates the empty shopping cart list declared earlier
-    print(f"{quantity} x {selectedItem[0]} added to shopping cart.") # Prints each item being added to the cart
+    print(f"{quantity} x {selectedItem[0]} @ £{selectedItem[1]:.2f} added to shopping cart.") # Prints each order line added to cart
 
-# Main Shopping loop | Complete nathan code and adam test 
-receipt = open("finalreceipt.txt", "wt") # Creates and opens the text file
-print("\nFinal Order Receipt:\n") # Prints the receipt header in the console 
-receipt.write("Final Order Receipt:\n\n") # Prints a the recipet header in to the text file.
-total = 0 # Sets total to zero to be updated in for loop below
-for item in shoppingCart: # For loop to go through each item in the shoppingCart
-    orderLine = item[1] * item[2] # Saves an order line total by working out price * qty
-    total = total + orderLine # Updates the total each loop with itself + price*qty total
-    print(f"{item[0]}(x{item[2]})                     £{orderLine:.2f}")            # Prints to terminal
-    receipt.write(f"{item[0]} (x{item[2]})                     £{orderLine:.2f}\n") # Write to file
+# Receipt file logic
+receipt = open("finalreceipt.txt", "wt")    # Creates and opens the text file
+print("\nOrder details:\n")                 # Prints the receipt header in the console 
+receipt.write("Receipt:\n\n")               # Prints a receipt header into the text file.
 
-print(f"\nThe total amount is: £{total:.2f}\n") # Print total amount to console
-receipt.write(f"\nThe total amount is: £{total:.2f}\n") # Write total amount to file
+total = 0   # Sets total to zero to be updated in the for loop below
 
-print(today.strftime("%d-%m-%Y %H:%M:%S")) # Print time to console
-receipt.write(today.strftime("%d-%m-%Y %H:%M:%S")) # Write time to file
-receipt.close() # Close the text file
+print("Item                     QTY    Cost (£)")              # Prints the receipt menu to console
+print("-" * 40)                                                # Prints menu separator to console
+receipt.write("Item                     QTY    Cost (£)\n")    # Prints the receipt menu to file
 
+for item in shoppingCart:                                                   # For loop to go through each item in the shoppingCart
+    orderLine = item[1] * item[2]                                           # Saves an order line total by working out price * qty
+    total = total + orderLine                                               # Updates the total each loop with itself + price*qty total
+    print(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}")                  # Prints each order line to terminal (with proper spacing from ><num)
+    receipt.write(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}\n")        # Writes each order line to file with same alignment (with proper spacing from ><num)
 
+print(f"\nTotal: {total:>30.2f}\n")                     # Write total amount to console (with proper spacing from ><num)
+receipt.write(f"\nTotal: {total:>30.2f}\n\n")           # Write total amount to file (with proper spacing from ><num)
 
+print(today.strftime("%d-%m-%Y %H:%M:%S"))              # Prints the time to the console, formatted in a user-friendly way
+receipt.write(today.strftime("%d-%m-%Y %H:%M:%S"))      # Write the time to the file, formatted in a user-friendly way
+
+receipt.close()                                         # Close the text file
