23f7dda7f431ca3c8ab583bbae3c1426dc777fdf
aj-mcneill <424603@nelsongroup.ac.uk>
2025-05-11 23:16:28 +0100

    Minor comment amends by Adam
    I corrected some incorrect comments, but there were no code changes.

--- main.py
+++ main.py
@@ -57,36 +57,36 @@ while True: # Opens an infinite loop (always True)
           print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.') # Warns the user of invalid quantity
           continue                     # Go back to the top of the loop
 
-    except ValueError:                 # If the conversion to integer fails
+    except ValueError:                                                                                        # If the conversion to integer fails
         print('Invalid quantity. Please enter at least 1 of the item you want or type "CHECKOUT" to finish.') # Warns the user of invalid quantity
-        continue                       # Go back to the top of the loop
+        continue                                                                                              # Go back to the top of the loop
 
     # Add item to cart
-    selectedItem = shoppingDict[itemCode] # Gets item from dictionary (using the item code the user typed)
-    shoppingCart.append((selectedItem[0], selectedItem[1], quantity))  # Populates the empty shopping cart list declared earlier
+    selectedItem = shoppingDict[itemCode]                                                     # Gets item from dictionary (using the item code the user typed)
+    shoppingCart.append((selectedItem[0], selectedItem[1], quantity))                         # Populates the empty shopping cart list declared earlier
     print(f"{quantity} x {selectedItem[0]} @ £{selectedItem[1]:.2f} added to shopping cart.") # Prints each order line added to cart
 
 # Receipt file logic
 receipt = open("finalreceipt.txt", "wt")    # Creates and opens the text file
 print("\nOrder details:\n")                 # Prints the receipt header in the console 
-receipt.write("Receipt:\n\n")               # Prints a receipt header into the text file.
+receipt.write("Receipt:\n\n")               # Writes the receipt header into the text file.
 
 total = 0   # Sets total to zero to be updated in the for loop below
 
-print("Item                     QTY    Cost (£)")              # Prints the receipt menu to console
-print("-" * 40)                                                # Prints menu separator to console
-receipt.write("Item                     QTY    Cost (£)\n")    # Prints the receipt menu to file
+print("Item                     QTY    Cost (£)")              # Prints the receipt menu header to the console
+print("-" * 40)                                                # Prints menu separator to the console
+receipt.write("Item                     QTY    Cost (£)\n")    # Writes the receipt menu header to the text file
 
 for item in shoppingCart:                                                   # For loop to go through each item in the shoppingCart
     orderLine = item[1] * item[2]                                           # Saves an order line total by working out price * qty
     total = total + orderLine                                               # Updates the total each loop with itself + price*qty total
-    print(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}")                  # Prints each order line to terminal (with proper spacing from ><num)
-    receipt.write(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}\n")        # Writes each order line to file with same alignment (with proper spacing from ><num)
+    print(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}")                  # Prints each order line to the terminal (with proper alignment using :><)
+    receipt.write(f"{item[0]:<25} {item[2]:<4} {orderLine:>6.2f}\n")        # Writes each order line to the text file (with proper alignment using :><)
 
-print(f"\nTotal: {total:>30.2f}\n")                     # Write total amount to console (with proper spacing from ><num)
-receipt.write(f"\nTotal: {total:>30.2f}\n\n")           # Write total amount to file (with proper spacing from ><num)
+print(f"\nTotal: {total:>30.2f}\n")                     # Prints the total amount to the console (with proper alignment using :><)
+receipt.write(f"\nTotal: {total:>30.2f}\n\n")           # Writes the total amount to the text file (with proper alignment using :><)
 
 print(today.strftime("%d-%m-%Y %H:%M:%S"))              # Prints the time to the console, formatted in a user-friendly way
-receipt.write(today.strftime("%d-%m-%Y %H:%M:%S"))      # Write the time to the file, formatted in a user-friendly way
+receipt.write(today.strftime("%d-%m-%Y %H:%M:%S"))      # Writes the time to the text file, formatted in a user-friendly way
 
 receipt.close()                                         # Close the text file
